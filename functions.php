15a892620dbcf182b0e84a904d09a503b213a5ac
ArtemSidorin <artemsidorin345@gmail.com>
2020-04-20 23:06:34 -1200

    script connection

--- functions.php
+++ functions.php
@@ -80,9 +80,7 @@ add_action( 'wp_enqueue_scripts', 'artbt_style');
 
 
 function artbt_scripts() {
-	wp_enqueue_script( 'artbt-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	wp_enqueue_script( 'artbt-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/script.js', array ('jquery'), '', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
